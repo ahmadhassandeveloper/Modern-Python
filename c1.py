4b71e691d7f107d368b34678cb76beabfedaccfa
ahmadhassandeveloper <ahmadhassanrazavu@gmail.com>
2023-10-23 20:58:02 +0500

    methods and attributes

--- c1.py
+++ c1.py
@@ -4,3 +4,4 @@ name: any = "Pakistan"
 print(name)             #print
 print(type(name))       #type
 print(id(name))         #physical address
+print([i for i in dir(name) if "__" not in i]) # methods and attributes
